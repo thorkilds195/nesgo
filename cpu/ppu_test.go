60c0e2cbdac10a6f82c5a36fe8560ae96cbf35e7
Thomas Thorkildsen <ThomasT@rystadenergy.com>
2025-04-20 18:30:00 +0200

    More tests for ppu

--- cpu/ppu_test.go
+++ cpu/ppu_test.go
@@ -101,6 +101,67 @@ func TestReadsValueFromChrSpace(t *testing.T) {
 	p.WriteToPPUAddr(0x15)
 	p.ReadData()
 	if !(p.ReadData() == 0x25) {
-		t.Error("PPU vram value not correct")
+		t.Error("Chr rom return value not correct")
+	}
+}
+
+func TestWritesToOAMAddr(t *testing.T) {
+	p := setupTestPPU(VERTICAL)
+	p.WriteToOAMAddr(0x25)
+	if !(p.oam_addr_reg == 0x25) {
+		t.Error("OAM addr value not correct")
+	}
+}
+func TestWritesToPPUCtrl(t *testing.T) {
+	p := setupTestPPU(VERTICAL)
+	p.WriteToPPUCtrl(0x25)
+	if !(p.ctrl.value == 0x25) {
+		t.Error("CTRL value not correct")
+	}
+}
+func TestWritesToScrollInXMode(t *testing.T) {
+	p := setupTestPPU(VERTICAL)
+	p.WriteToScroll(0x25)
+	if !(p.scroll.x_value == 0x25) {
+		t.Error("Scroll x_value not correct")
+	}
+}
+
+func TestWritesToScrollInYMode(t *testing.T) {
+	p := setupTestPPU(VERTICAL)
+	p.WriteToScroll(0x15)
+	p.WriteToScroll(0x25)
+	if !(p.scroll.x_value == 0x15 && p.scroll.y_value == 0x25) {
+		t.Error("Scroll values not correct")
+	}
+}
+func TestReadsStatusRegister(t *testing.T) {
+	p := setupTestPPU(VERTICAL)
+	p.status.value = 0b1100_1000
+	if !(p.ReadStatusRegister() == 0b1100_1000 &&
+		!p.status.isVblankSet() &&
+		p.addr.is_hi &&
+		p.scroll.is_x) {
+		t.Error("Wrong status register value")
+	}
+}
+
+func TestInteractsWithOAMRegister(t *testing.T) {
+	p := setupTestPPU(VERTICAL)
+	p.WriteOAMData(0x25)
+	if !(p.oam_data[0] == 0x25) {
+		t.Error("Wrong value for OAM data write")
+	}
+	p.oam_data[1] = 0x15
+	if !(p.ReadOAMData() == 0x15) {
+		t.Error("Wrong value for OAM data read")
+	}
+}
+
+func TestWritesToMask(t *testing.T) {
+	p := setupTestPPU(VERTICAL)
+	p.WriteToMask(0b0001_1000)
+	if !(p.mask.value == 0b0001_1000) {
+		t.Error("Wrong value for mask data write")
 	}
 }
